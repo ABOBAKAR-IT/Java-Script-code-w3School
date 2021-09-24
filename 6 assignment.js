dd11b158902097766aa5be7b35b783a50b72d9c3
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2021-09-24 12:08:08 +0500

    assignment operator

---
+++ 6 assignment.js
@@ -0,0 +1,17 @@
+// ********Assignment operator*******
+console.log(" *******Assignment operator******* \n")
+let a=10;
+let b=5;
+a+=b;
+console.log("a+=b ",a);
+a-=b;
+console.log("a-=b ",a);
+a*=b;
+console.log("a*=b ",a);
+a/=b;
+console.log("a/=b ",a);
+a**=b;
+console.log("a**=b ",a);
+a%=b;
+console.log("a%=b ",a);
+
