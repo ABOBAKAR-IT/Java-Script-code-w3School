268e0719f752c4dab67807f146cda4488e26e987
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2021-09-24 12:03:19 +0500

    variables

---
+++ 1 Variables.js
@@ -0,0 +1,24 @@
+var a=10;
+var b=12;
+b=b+a;
+
+console.log(b);
+b='rana';
+console.log(b);
+b=1.23;
+console.log(b);
+console.log(a,a);
+var a=4,c='ranag';
+console.log(a,c);
+var a;//If you re-declare a JavaScript variable, 
+console.log(a);//it will not lose its value.
+var d;//A variable declared without a value will have the value undefined.
+console.log(d);
+var x =5+ 3 + '3';//If you put a number in quotes,in last firat plus 5+3=8 and 3 add as a string output is 83
+console.log(x);
+var y= '5'+ 3 + 3;//If you put a number in quotes, in first , then all add as a string output is 533
+console.log(y);
+var $='rana';//JavaScript Dollar Sign $
+console.log($);
+var _=2;//JavaScript Underscore (_)
+console.log(_);
